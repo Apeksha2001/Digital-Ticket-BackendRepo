d03d491d6e4c2c4ba76ef4a564bd6674c0110920
Apeksha2001 <apekshadhormale28@gmail.com>
2023-02-27 12:53:09 +0530

    ticketAPI

---
+++ routes/ticket.route.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const TicketController=require('../controller/ticket.controller')
+const router = require("express").Router();
+
+
+//Add ticket 
+router.post('/addTicket',TicketController.postTicketDetails)
+
+//Getting details about ticket
+router.get('/getTicket',TicketController.getTicketDetails);
+
+//Getting details about particular ticket
+router.get('/getOneTicketDetail/:id',TicketController.getOneTicketInfo);
+
+//Deleting Ticket Details
+
+router.delete('/deleteTicket/:id',TicketController.deleteTicketDetails)
+
+module.exports = router;
